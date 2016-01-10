b6e3d9fffecc5d5bd98800f5803bfa1a34464645
szonjao <szonja.o@me.com>
2016-01-10 18:25:41 +0100

    trial for display

--- apps/app.js
+++ apps/app.js
@@ -2,6 +2,7 @@
 //Places: AIzaSyB-oHRgiQ7xP9S1Y3VoTtUPLHDQ0EaYI3E
 
 var map;
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map_area'), {
     	center: {lat: -34.397, lng: 150.644},
@@ -55,6 +56,27 @@ function initMap() {
       }
     });
     map.fitBounds(bounds);
+
+
+    
+function initService() {
+  var displaySuggestions = function(predictions, status) {
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+      alert(status);
+      return;
+    }
+
+    predictions.forEach(function(prediction) {
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(prediction.description));
+      document.getElementById('details_area').appendChild(li);
+    });
+  };
+
+  var service = new google.maps.places.AutocompleteService();
+  service.getQueryPredictions({ input: 'pizza near Syd' }, displaySuggestions);
+}
   });
 
 }
+
